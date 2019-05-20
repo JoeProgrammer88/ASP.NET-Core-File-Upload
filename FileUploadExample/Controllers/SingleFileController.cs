1f8074117407d090aea64d8b8b413dae9f889fe1
Joseph Ortiz <joseph.ortiz@cptc.edu>
2019-05-20 16:38:00 -0700

    Add sample entity to upload file

---
+++ FileUploadExample/Controllers/SingleFileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadExample.Controllers
+{
+    public class SingleFileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
