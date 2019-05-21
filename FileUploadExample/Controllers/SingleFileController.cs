72363db89d16f706b9c7d90133ad5493278ee412
Joseph Ortiz <joseph.ortiz@cptc.edu>
2019-05-20 17:16:48 -0700

    Photo uploading to server

--- FileUploadExample/Controllers/SingleFileController.cs
+++ FileUploadExample/Controllers/SingleFileController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FileUploadExample.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileUploadExample.Controllers
@@ -10,6 +11,29 @@ namespace FileUploadExample.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Food item)
+        {
+            //Either use a viewmodel or remove photoURL. URL will
+            //be generated programmatically
+            ModelState.Remove(nameof(Food.PhotoUrl));
+            item.Id = 0; //SQL Server will generate a new ID
+
+            if (ModelState.IsValid)
+            {
+                //Check file extension is a photo
+
+                //reduce photo size
+
+                //generate unique name to retrieve later
+
+                //store photo on file system and reference in DB
+                return View();
+            }
+
             return View();
         }
     }
--- FileUploadExample/Models/Food.cs
+++ FileUploadExample/Models/Food.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace FileUploadExample.Models
 {
     public class Food
     {
+        [Key]
         public int Id { get; set; }
 
         public string Name { get; set; }
